d00c111acb527300917f6944815c5c6b3d923948
Sebastien  Lourdel <slourdelmacbook@AirdeSebastien.lan>
2022-10-12 03:53:18 +0200

    css v1.1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <!-- Header section -->
 <header>
     <h1>
-      <img src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
+      <img class="wild" src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
       Les Argonautes
     </h1>
     <link rel="stylesheet" type="text/css" href="style.css"/>
